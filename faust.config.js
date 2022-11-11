cbcd543a5cba18659e01af46e0a27f61bd1486fb
Herb Miller <herb@bobbingwide.com>
2022-11-11 17:36:21 +0000

    Workaround for SSL certificate issues

--- faust.config.js
+++ faust.config.js
@@ -2,6 +2,8 @@ import { setConfig } from '@faustwp/core';
 import templates from './wp-templates';
 import possibleTypes from './possibleTypes.json';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 /**
  * @type {import('@faustwp/core').FaustConfig}
  **/
